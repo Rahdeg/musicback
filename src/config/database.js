3408247eff6de16ad1bf77382e4d0208bf3e6196
Rahdeg <walett95@gmail.com>
2023-01-20 18:03:57 +0100

    rewt

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const port = process.env.PORT || 5000;
 const connectDatabase = async function(app){
     try {
-        mongoose.connect(process.env.DB_URI, ()=>{
+        mongoose.connect('mongodb+srv://musicapp:musicapp@cluster0.eqcfh.mongodb.net/?retryWrites=true&w=majority', ()=>{
             console.log("Database Connected");
             app.listen(port, ()=>{
                 console.log(`Server listening on ${port}`);
