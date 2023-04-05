8a06d28a401baa30d4223e177b33ec495f2f6ce0
rahdeg <walett95@gmail.com>
2023-04-05 12:26:59 +0100

    firebase update

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const port = process.env.PORT || 5000;
 const connectDatabase = async function(app){
     try {
-        mongoose.connect('mongodb+srv://musicapp:musicapp@cluster0.eqcfh.mongodb.net/?retryWrites=true&w=majority', ()=>{
+        mongoose.connect('mongodb+srv://musicapp:musicapp@cluster0.eqcfh.mongodb.net/?retryWrites=true&w=majority',{dbName:"Mymusic"},()=>{
             console.log("Database Connected");
             app.listen(port, ()=>{
                 console.log(`Server listening on ${port}`);
